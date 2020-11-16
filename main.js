627cd13d9168b9189bb59c5faa2a22e616bb7d62
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-11-16 15:44:07 -0800

    add animal name generator

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-const random_name = require('node-random-name');
-console.log(random_name());
+const randomName = require('node-random-name');
+console.log(randomName());
+
+const randomAnimal = require('random-animal-name-generator');
+console.log(randomAnimal());
