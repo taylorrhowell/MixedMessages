64abf2774ebc2499007c66d6bab75c0afb7ff7ab
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-11-16 16:12:50 -0800

    create action generator

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const randomName = require('node-random-name');
-const randomAnimal = require('random-animal-name-generator');
 
+const randomAnimal = require('random-animal-name-generator');
 const nameWithAdj = randomAnimal();
 const animalName = nameWithAdj.split(' ')[1];
+
+const activityList = ['jumps', 'skips', 'runs', 'falls', 'leaps', 'stands', 'drives', 'sleeps', 'wrestles', 'dances', 'loses fingers', 'trapped alone', 'arrested', 'crashes', 'plays games', 'eats breakfast', 'eats lunch', 'eats dinner', 'eats snack', 'eats dessert', 'dives'];
+const generateActivity = () => {
+    const randomIndex = Math.floor(Math.random() * activityList.length);
+    const activity = activityList[randomIndex];
+    return activity;
+};
